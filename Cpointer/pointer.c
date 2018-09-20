4cb658b2daf40894b5f491908b032e00314ef723
oleen <1621240166@qq.com>
2018-09-20 20:17:59 +0800

    add Cpointer folder

---
+++ Cpointer/pointer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char **argv) 
+{
+	printf("sizeof(char) = %d\n", sizeof(char));
+	printf("sizeof(char) = %d\n", sizeof(char));
+	printf("sizeof(char) = %d\n", sizeof(char));
+	printf("sizeof(char) = %d\n", sizeof(char));
+	return 0;
+}
